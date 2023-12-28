72c200dfe3532e7ef5dd95d65f24a3fdc6a65d2e
kex123d <155063172+kex123d@users.noreply.github.com>
2023-12-28 14:53:00 +0200

    Create saveUserData.js

---
+++ api/saveUserData.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    let data = '';
+
+    req.on('data', chunk => {
+      data += chunk;
+    });
+
+    req.on('end', () => {
+      try {
+        const jsonData = JSON.parse(data);
+        const userData = `${jsonData.username}:${jsonData.key};\n`;
+
+        fs.appendFile('.well-known/nostr.json', userData, err => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ message: 'Internal Server Error' });
+          } else {
+            res.json({ message: 'Data saved successfully' });
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Invalid data format' });
+      }
+    });
+  } else {
+    res.status(405).end(); // Method Not Allowed
+  }
+};
